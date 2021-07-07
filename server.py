1f99b8a79c9256a154c879be7aaa10191b302f38
FarazMaqsood <farazmaqsood@A006-00180.arbisoft.com>
2021-07-07 16:13:38 +0500

    code updated

--- server.py
+++ server.py
@@ -2,15 +2,17 @@ import json
 file1 = open("info_about_lunch.txt","r")
 dic1=file1.read()
 dic2=json.loads(dic1)
+
 print("if you want to check the menu for ten days, Select numbers from 0-9 \nif you want to exit press any other number")
-a=None
+a=0
 b=""
 c=[]
-while(a>=0 and a<=9 or a==None):
+
+while(int(a)>=0 and int(a)<=9):
     a = input()
-    if a>=0 and a<=9:
-        aa=str(a)
-        b = dic2[aa]
+    
+    if int(a)>=0 and int(a)<=9:
+        b = dic2[a]
         c=b.split("-")
         print("The day You selected is "+c[0]+" and the lunch today is "+ c[1]+"\n")
         print("Again, if you want to check the menu for ten days, Select numbers from 0-9 \nif you want to exit press any other number")
