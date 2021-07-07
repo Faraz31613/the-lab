a54a6fa0855ad1c70568fdc3d897175fc5246769
FarazMaqsood <farazmaqsood@A006-00180.arbisoft.com>
2021-07-07 17:35:49 +0500

    changed the name of file1,made the file manager class for closig file,captalize the i in if u want,changed a name of value,

--- server.py
+++ server.py
@@ -1,20 +1,24 @@
 import json
-file1 = open("info_about_lunch.txt","r")
-data_string=file1.read()
+from file_manager import FileManager
+
+data_string = ""
+with FileManager('info_about_lunch.txt', 'r') as lunch_details_file:
+    data_string=lunch_details_file.read()
+
 data_dictionary=json.loads(data_string)
 
-print("if you want to check the menu for ten days, Select numbers from 0-9 \nif you want to exit press any other number")
+print("If you want to check the menu for ten days, Select numbers from 0-9 \nIf you want to exit press any other number")
 key=0
-value=""
-value_list=[]
+lunch_according_day=""
+lunch_according_day_list=[]
 
 while(int(key)>=0 and int(key)<=9):
     key = input()
     
     if int(key)>=0 and int(key)<=9:
-        value = data_dictionary[key]
-        value_list=value.split("-")
-        print("The day You selected is "+value_list[0]+" and the lunch today is "+ value_list[1]+"\n")
+        lunch_according_day = data_dictionary[key]
+        lunch_according_day_list=lunch_according_day.split("-")
+        print("The day You selected is "+lunch_according_day_list[0]+" and the lunch today is "+ lunch_according_day_list[1]+"\n")
         print("____________________\n") #adding a seperating line between questions asked
-        print("Again, if you want to check the menu for ten days, Select numbers from 0-9 \nif you want to exit press any other number")
+        print("Again, If you want to check the menu for ten days, Select numbers from 0-9 \nIf you want to exit press any other number")
 
