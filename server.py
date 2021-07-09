70780709fe58fe0e41b69695374c59a3c787b381
FarazMaqsood <farazmaqsood@A006-00180.arbisoft.com>
2021-07-09 12:25:04 +0500

    I made the server with GET reqest by passing the number in URL as parameter and server tells the lunch in response

--- server.py
+++ server.py
@@ -1,10 +1,33 @@
-from http.server import HTTPServer, BaseHTTPRequestHandler
-from urllib.parse import parse_qs
 import json
+from urllib.parse import parse_qs
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
 from file_manager import FileManager
 
 
 class MessageHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        # First, send a 200 OK response.
+        self.send_response(200)
+
+        # Then send headers.
+        self.send_header('Content-type', 'text/plain; charset=utf-8')
+        self.end_headers()
+
+        # Now, write the response body.
+        menu_number = self.path[1:]
+
+        data_string = ""
+        with FileManager('info_about_lunch.txt', 'r') as lunch_details_file:
+            data_string=lunch_details_file.read()
+
+        data_dictionary=json.loads(data_string)
+
+        lunch_according_day = data_dictionary[menu_number]
+        lunch_according_day_list=lunch_according_day.split("-")
+        menu_u_selected="The day You selected is "+lunch_according_day_list[0]+" and the lunch today is "+ lunch_according_day_list[1]+"\n"
+        self.wfile.write(menu_u_selected.encode())
+
     def do_POST(self):
         # 1. How long was the message? (Use the Content-Length header.)
         length = int(self.headers.get('Content-length', 0))
@@ -37,3 +60,4 @@ if __name__ == '__main__':
     server_address = ('', 8080)
     httpd = HTTPServer(server_address, MessageHandler)
     httpd.serve_forever()
+    
