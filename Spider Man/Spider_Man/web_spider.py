f6cbfa83aea07684b48dc5a30e335f200654c8b7
FarazMaqsood <farazmaqsood@A006-00180.arbisoft.com>
2021-07-28 19:13:50 +0500

    format web_spider.py

--- Spider Man/Spider_Man/web_spider.py
+++ Spider Man/Spider_Man/web_spider.py
@@ -8,25 +8,20 @@ class WebSpiderSpider(scrapy.Spider):
     name = 'web_spider'
     allowed_domains = ['wikipedia.org']
     start_urls = ['http://wikipedia.org/']
-    url=""
+    url = ""
 
-    
     def parse(self, response):
-        
-        i=0
+
+        i = 0
         res_urls = response.css('a::attr(href)').extract()
         while i < len(res_urls):
             if res_urls[i].startswith("//"):
                 res_urls[i] = "https:" + res_urls[i]
             elif not res_urls[i].startswith("//"):
                 res_urls[i] = self.url + res_urls[i]
-            url = {
-                i : res_urls[i]
-            }
+            url = {i: res_urls[i]}
             # closespider_itemcount = 500
             yield url
             self.url = res_urls[i]
-            yield scrapy.Request(url=res_urls[i],callback = self.parse)
-            i=i+1
-
-        
+            yield scrapy.Request(url=res_urls[i], callback=self.parse)
+            i = i + 1
