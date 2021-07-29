1f90a4068f5b6c62d6042e404e834a1ff0a0ce31
FarazMaqsood <farazmaqsood@A006-00180.arbisoft.com>
2021-07-29 18:57:37 +0500

    create a scrapy projects today in which we can specifies no.of concurrent_requests, download delay and limit the number of visited urls

--- Spider Man/Spider_Man/web_spider.py
+++ Spider Man/Spider_Man/web_spider.py
@@ -25,3 +25,24 @@ class WebSpiderSpider(scrapy.Spider):
             self.url = res_urls[i]
             yield scrapy.Request(url=res_urls[i], callback=self.parse)
             i = i + 1
+
+    # name = 'web_spider'
+    # allowed_domains = ['wikipedia.org']
+    # start_urls = ['http://wikipedia.org/']
+    # url = ""
+    # i=0
+    # def parse(self, response):
+
+    #     #i = 0
+    #     res_urls = response.css('a::attr(href)').extract()
+    #     while (res_urls[self.i]):
+    #         if res_urls[self.i].startswith("//"):
+    #             res_urls[self.i] = "https:" + res_urls[self.i]
+    #         elif not res_urls[self.i].startswith("//"):
+    #             res_urls[self.i] = self.url + res_urls[self.i]
+    #         url = {self.i: res_urls[self.i]}
+    #         # closespider_itemcount = 500
+    #         yield url
+    #         self.url = res_urls[self.i]
+    #         self.i = self.i + 1
+    #         yield scrapy.Request(url=res_urls[self.i-1], callback=self.parse)
