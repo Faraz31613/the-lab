8edb9cc5afa742b02986638affa5da291861a677
FarazMaqsood <farazmaqsood@a006-00180.home>
2021-08-18 14:55:50 +0500

    try a todos app

--- To_Do_List_React_App/to_do_list/src/toDoApp.js
+++ To_Do_List_React_App/to_do_list/src/toDoApp.js
@@ -1,28 +1,60 @@
-import React, {useState} from "react"
-import Tasks from "./tasks"
+import React, { useState } from "react";
+// import Tasks from "./tasks";
 
-export default function ToDoApp(){
+export default function ToDoApp() {
+  const initialState = [];
+  const [tasks, addTask] = useState(initialState);
+  const [todo, getToDo] = useState([]);
 
-    const initialState = []
-    const [tasks,addTask] = useState(initialState)
-
-    function addToDoTask(event){
-        const task = document.getElementsByClassName("tasks")[0].value
-        if (task!==""){
-            addTask(prevTasks => [...prevTasks,task])
-            document.getElementsByClassName("tasks")[0].value = ""
-        }
+  function handleChangeTask(event) {
+    const { name, value, type ,checked} = event.target;
+    if (type === "checkbox") {
+        // addTask(prevTasks => prevTasks.map((prevTask)=>{
+        //     if(prevTask["work"]===value){
+        //         prevTask["completed"]==!prevTask["completed"]
+        //     }
+        //     return prevTask
+        // }))
+    }else{
+        getToDo((prevToDo) => ({ work: value, completed: false }));
     }
+    console.log(tasks)
+  }
 
-    return (
-        <div>
-            <div>
-            <input className="tasks" name="task" placeholder="What needs to be done?  " />
-            <button className="addTaskBtn" onClick={addToDoTask}>Add Task</button>
-            </div>
-            <div className='taskSheet'>
-                <Tasks tasks={tasks}/>
-            </div>
-        </div>
-    )
+  function addToDoTask(event) {
+    if (todo["work"] !== "") {
+      addTask((prevTasks) => [...prevTasks, todo]);
+    }
+  }
+
+  return (
+    <div>
+      <div>
+        <input
+          type="text"
+          className="tasks"
+          name="task"
+          placeholder="What needs to be done?  "
+          onChange={handleChangeTask}
+        />
+        <button className="addTaskBtn" onClick={addToDoTask}>
+          Add Task
+        </button>
+      </div>
+      <div className="taskSheet">
+        {/* <Tasks tasks={tasks}/> */}
+        {tasks.map((task) => (
+          <div className="contentDiv">
+            <input
+              className="addedTaskBox"
+              type="checkbox"
+              value={task["work"]}
+              onChange={handleChangeTask}
+            />
+            <label className="addedTask">{task["work"]}</label>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
