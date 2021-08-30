c1d371c5e10f6c7ddd151dac24ef7216852f3578
FarazMaqsood <farazmaqsood@A006-00180.arbisoft.com>
2021-08-30 10:11:19 +0500

    home page authentication

--- REST_based_Auth_API/facebook_replica/src/components/Home.js
+++ REST_based_Auth_API/facebook_replica/src/components/Home.js
@@ -3,7 +3,10 @@ import { useDispatch, useSelector } from "react-redux";
 import { Redirect } from "react-router-dom";
 
 const Home = () => {
-  const isSignedIn = useSelector((state) => state.signIn);
+  const isSignedIn = useSelector((state) => state.signIn["isSignedIn"]);
+  if(!isSignedIn){
+    return <Redirect to="/signIn" />
+  }
 
   return <h3>Home</h3>;
 };
